644fb0f6fc921782079da0d4072e03590d5761b3
Steve KINGSLEY ARULNEASATHAS <steve.kingsleyarulneasathas@edu.univ-paris13.fr>
2025-12-03 12:39:54 +0100

    AJout de certaines fonctions (non définies), et avec un peu de commentaire

--- code/algos.py
+++ code/algos.py
@@ -4,14 +4,41 @@ from fonctions_annexes import *
 
 # Steve fait cet algo
 def balas_hammer() :
-
     return 0
 
 
 
 # Matthew fait cet algo
 def nord_ouest() :
-
     return 0 # Modifier la valeur de retour par celle à vraiment retourner (s'il faut retourner une valeur)
 
 
+#
+# Méthode du marche-pied avec potentiel
+#
+def marche_pied_potentiel() :
+    # Affichage de la proposition de transport, ainsi que son coût de transport total
+    # Test pour savoir si la proposition de transport est dégénérée
+    
+    # Modifications du graphe de transport pour obtenir un arbre, dans les cas cyclique ou non connexe.
+    
+    # Calcul et affichage des potentiels
+    
+    return 0
+
+
+# VÉRIFIER QUE C'EST CORRECT
+def calcul_couts_marginaux(matrice_couts, matrice_couts_potentiels) :
+    i = 0
+    j = 0
+
+    couts_marginaux = []
+
+    for i in range (len(matrice_couts)) :
+        ligne = []
+        for j in range (len(matrice_couts_potentiels)) :
+            ligne.append(matrice_couts[i][j] - matrice_couts_potentiels[i][j])
+        couts_marginaux.append(ligne)
+    
+    
+    return couts_marginaux
