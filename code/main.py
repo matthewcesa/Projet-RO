8a09517ce0bc1ec18d44b800725b6961a405962d
Constant NGUYEN <constant.nguyen@iut-rodez.fr>
2025-12-06 20:17:27 +0100

    Algo de balas-hammer

--- code/main.py
+++ code/main.py
@@ -21,8 +21,31 @@ def boucle_principale() :
 
 
 if __name__ == "__main__" :
-    pb_a_traiter = demander_pb_a_traiter()
-    while(pb_a_traiter != 0) :
-        boucle_principale()
+    matrice_couts = [
+        [30, 20, 20],   # ligne 0
+        [10, 50, 20],   # ligne 1
+        [50, 40, 30]    # ligne 2
+    ]
 
-        pb_a_traiter = demander_pb_a_traiter()
+
+    print("\n MATRICE INITIALE COUT")
+    for ligne in matrice_couts:
+        print(ligne)
+
+    matrice_provisions = [
+        [math.inf, math.inf, math.inf, 450], 
+        [math.inf, math.inf, math.inf, 600], 
+        [math.inf, math.inf, math.inf, 350],   
+        [500,      600,       300,    1400]  
+    ]
+
+    
+
+    print("\n MATRICE INITIALE PROIVISONS")
+    for ligne in matrice_provisions:
+        print(ligne)
+
+    resultat = balas_hammer(matrice_provisions, matrice_couts)
+    print("\nRÉSULTAT MATRICE PROVISIONS")
+    for ligne in resultat:
+        print(ligne)
