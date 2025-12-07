101b51ab23562359388e999769c3d41388126fd8
Steve KINGSLEY ARULNEASATHAS <steve.kingsleyarulneasathas@edu.univ-paris13.fr>
2025-12-07 19:46:04 +0100

    J'ai commencé à donner forme au main

--- code/main.py
+++ code/main.py
@@ -1,31 +1,35 @@
 from algos import *
-from Probleme import *
-
-
-def boucle_principale() :
-    # Lire le tableau et stocker en mémoire (à faire par Matthew)
-    
-    matrice_1 = [ # Matrice de test
-        [30,20,100],
-        [10,50,100],
-        [100,100]
-    ]
-    
-
-    choix_algo = demander_algo()
-
-    if(choix_algo == 1) :
-        # Nord-Ouest
-        print("Vous avez choisi Nord-Ouest")
-    else : # choix_algo = 2
-        print("Vous avez choisi Balas-Hammer")
 
 
 if __name__ == "__main__" :
-    p = Probleme(3,3)
-    afficher_matrice(p.couts)
-    # pb_a_traiter = demander_pb_a_traiter()
-    # while(pb_a_traiter != 0) :
-        # boucle_principale()
+    probleme = demander_pb_a_traiter()
+    while (probleme != 0) :
+        # Lire le tableau dans le fichier correspondant
+        couts = [
+            [11,12,10,10],
+            [17,16,15,18],
+            [19,21,20,22]
+        ]
+        print("Matrice des coûts :")
+        afficher_matrice(couts, len(couts), len(couts[0]))
 
-        # pb_a_traiter = demander_pb_a_traiter()
+        proposition_transport = [
+            [0,0,0,0,60],
+            [0,0,0,0,30],
+            [0,0,0,0,90],
+            [50,75,30,25]
+        ]
+
+        proposition_transport[len(proposition_transport)-1].append(calculer_somme_transport(proposition_transport))
+
+        print("Proposition de transport :")
+        afficher_matrice(proposition_transport, len(proposition_transport), len(proposition_transport[0]))
+
+        choix_algo = demander_algo()
+        if(choix_algo == 1) :
+            # Nord-Ouest
+            print("Vous avez choisi Nord-Ouest")
+            break
+        else : # choix_algo = 2
+            print("Vous avez choisi Balas-Hammer")
+            break
