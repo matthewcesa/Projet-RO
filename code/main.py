a14db5579951610e033e25a25fca4d28352ac5cf
Stevekgs28 <84630495+Stevekgs28@users.noreply.github.com>
2025-12-08 17:43:16 +0100

    Suppression de fonction inutile

--- code/main.py
+++ code/main.py
@@ -1,25 +1,6 @@
 from algos import *
 
 
-def boucle_principale() :
-    # Lire le tableau et stocker en mémoire (à faire par Matthew)
-    
-    matrice_1 = [ # Matrice de test
-        [30,20,100],
-        [10,50,100],
-        [100,100]
-    ]
-    
-
-    choix_algo = demander_algo()
-
-    if(choix_algo == 1) :
-        # Nord-Ouest
-        print("Vous avez choisi Nord-Ouest")
-    else : # choix_algo = 2
-        print("Vous avez choisi Balas-Hammer")
-
-
 if __name__ == "__main__" :
     matrice_couts = [
         [30, 20, 20],   # ligne 0
