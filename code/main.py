d037b794f33189fd13c7dc952873ca5e4cbfe021
Steve KINGSLEY ARULNEASATHAS <steve.kingsleyarulneasathas@edu.univ-paris13.fr>
2025-12-10 14:18:22 +0100

    Complétion du code

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 from marchepied import *
 from nord_ouest import *
+from balas_np import *
 
 
 if __name__ == "__main__" :
@@ -26,15 +27,25 @@ if __name__ == "__main__" :
         print("Proposition de transport :")
         afficher_matrice(proposition_transport, len(proposition_transport), len(proposition_transport[0]))
 
+        print()
         choix_algo = demander_algo()
+        print()
+
         if(choix_algo == 1) :
             # Nord-Ouest
             print("Vous avez choisi Nord-Ouest")
             matrice = algorithme_nord_ouest(proposition_transport)
-
-            afficher_matrice(matrice, len(matrice), len(matrice[0]))
+            copier_tableau(matrice,proposition_transport) # La fonction de Matthew était implémentée un peu différemment, donc il a fallu faire un rapide ajustement
+            print("Proposition de transport avec Nord-Ouest :")
+            afficher_matrice(proposition_transport, len(proposition_transport), len(proposition_transport[0]))
 
         else : # Si choix_algo = 2
             print("Vous avez choisi Balas-Hammer")
+            proposition_transport = balas_hammer(proposition_transport, couts)
+            print("Proposition de transport avec Balas-Hammer :")
+            afficher_matrice(proposition_transport, len(proposition_transport), len(proposition_transport[0]))
+
 
         marche_pied_potentiel(couts, proposition_transport)
+    
+    print("Vous ne souhaitez plus tester de nouveaux problèmes.")
