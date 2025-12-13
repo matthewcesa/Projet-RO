acd1727f1ec32f43cb7b1549dc346112e199240d
Constant NGUYEN <constant.nguyen@iut-rodez.fr>
2025-12-13 20:01:25 +0100

    MAJ

--- code/balas.py
+++ code/balas.py
@@ -1,169 +1,100 @@
 from fonctions_annexes import *
+import numpy as np
 import math
 
-def calcul_delta_lignes(matrice_couts, colonnes_actives, num_ligne):
-
-    valeurs = [matrice_couts[num_ligne][j] for j in colonnes_actives]
-
+def delta_deux_plus_petites(valeurs):
+    """Calcule le delta entre les deux plus petites valeurs d'une liste"""
     if len(valeurs) < 2:
-        return -math.inf
+        return -math.inf  # Si une seule valeur, on retourne inf car delta pas calculable
+    # prend les deux plus petites valeurs, partition avec le 1 permet de trier le tableau et le [:2] de prendre que les 2 première valeurs
+    deux_plus_petites = np.partition(valeurs, 1)[:2]  
+    return deux_plus_petites[1] - deux_plus_petites[0]
 
-    valeurs = sorted(valeurs)
-    return valeurs[1] - valeurs[0]
 
+# Constant fait cet algo
+def balas_hammer(matrice_provisions, matrice_couts):
 
-def calcul_delta_colonnes(matrice_couts, lignes_actives, num_colonne):
-    valeurs = [matrice_couts[i][num_colonne] for i in lignes_actives]
+    # Convertir en tableau numpy car plus simple à utiliser
+    couts = np.array(matrice_couts, dtype=float)
+    provisions = np.array(matrice_provisions, dtype=float)
 
-    if len(valeurs) < 2:
-        return -math.inf
+    # permet de donner le nbr de ligne et de colonne dans le tableau
+    n_lignes, n_colonnes = couts.shape
 
-    valeurs = sorted(valeurs)
-    return valeurs[1] - valeurs[0]
+    # On garde les valeurs de la dernière colonne et dernière ligne qui sont les totaux 
+    total_lignes_restantes = provisions[:n_lignes, -1].copy()
+    total_colonne_restantes = provisions[-1, :n_colonnes].copy()
 
-def balas_hammer(matrice_provision, matrice_cout):
+    # Matrice résultat que je retuorne à la fin
+    resultat = provisions.copy()  # on ne touche pas aux dernières lignes/colonnes
 
-    nb_lignes = len(matrice_cout)
-    nb_colonnes = len(matrice_cout[0])
-
-    # Sont toutes vides donc actives au début
-    lignes_actives = set(range(nb_lignes))
-    colonnes_actives = set(range(nb_colonnes))
+    # Lignes et colonnes actives (celles qui sont pas remplises) 
+    lignes_actives = set(range(n_lignes))
+    colonnes_actives = set(range(n_colonnes))
 
     compteur = 0
 
-    # Boucle sur ligne et colonne qui ne sont pas remplises
+    # Boucle tant que toute les lignes et colonnes sont pas remplises
     while lignes_actives and colonnes_actives:
 
-        print("\nACTIVEES à itération : ", compteur)    
-        print("\nLignes actives : ", lignes_actives)
-        print("Colonnes actives : ", colonnes_actives)
+        compteur +=1
+        # Calcul des deltas pour chaque ligne et colonne active
+        delta_lignes = {i: delta_deux_plus_petites(couts[i, list(colonnes_actives)]) 
+                        for i in lignes_actives}
+        delta_colonnes = {j: delta_deux_plus_petites(couts[list(lignes_actives), j]) 
+                          for j in colonnes_actives}
 
+        # Trouver le delta maximum pour ligne et colonne
+        max_delta_ligne = max(delta_lignes.values())
+        max_delta_colonne = max(delta_colonnes.values())
         
-        deltas_lignes = {}
-        for i in lignes_actives:
-            deltas_lignes[i] = calcul_delta_lignes(matrice_cout, colonnes_actives, i)
-
-        deltas_colonnes = {}
-        for j in colonnes_actives:
-            deltas_colonnes[j] = calcul_delta_colonnes(matrice_cout, lignes_actives, j)
-
+        print("\nItération :", compteur)     
+        print("Max colonnes :", delta_colonnes)
         
 
-        # Trouve meilleure ligne en calculant le delta
-        premiere_ligne = next(iter(deltas_lignes))
-        max_delta_ligne = deltas_lignes[premiere_ligne]
-        meilleure_ligne = [premiere_ligne, max_delta_ligne]
-        for ligne, delta in deltas_lignes.items():
-            if delta > max_delta_ligne:
-                max_delta_ligne = delta
-                meilleure_ligne = [ligne, delta]
-
-
-        # Trouve meilleure colonneen calculant le delta
-        premiere_colonne = next(iter(deltas_colonnes))
-        max_delta_colonne = deltas_colonnes[premiere_colonne]
-        meilleure_colonne = [premiere_colonne, max_delta_colonne]
-        for colonne, delta in deltas_colonnes.items():
-            if delta > max_delta_colonne:
-                max_delta_colonne = delta
-                meilleure_colonne = [colonne, delta]
-
-
-
-        # Vérifie si le delta le plus faible est ligne ou colonne
+        # Choix entre ligne et colonne selon delta maximum
         if max_delta_ligne >= max_delta_colonne:
-            # on traite la ligne sélectionnee
-            i = meilleure_ligne[0]
+            # La ligne a le delta le plus élevé
+            ligne_choisie = max(delta_lignes.items(), key=lambda x: x[1])[0]
+            print("\n C'est quoi çaaaaaaaaaaa : ", max(delta_lignes.items(), key=lambda x: x[1]))
+            # On prend la colonne avec le coût minimal (si il y a 2 delta le meme, on prend celle avec le plus petit)
+            colonne_choisie = min(colonnes_actives, key=lambda c: couts[ligne_choisie, c])
+            print("\n C'est quoi çaaaaaaaaaaaaaa V2: ", min(colonnes_actives, key=lambda c: couts[ligne_choisie, c]))
             
-            cout_min = math.inf
-            for j in colonnes_actives:
-                c = matrice_cout[i][j]
-                if c < cout_min:
-                    cout_min = c
-                    index_col_choisie = j
-
-            offre = matrice_provision[i][-1]
-            demande = matrice_provision[-1][index_col_choisie]
-
-            # la quantite max qu'on peut mettre dans la case minimale du delta le plus élevé (ca va pas chiant comme phrase)
-            quantite = min(offre, demande)
-
-            # On met la valeur dans la bonne case
-            matrice_provision[i][index_col_choisie] = quantite
-
-            # On met les autre cases de la colone à 0 
-            for ligne_idx in range(len(matrice_provision) - 1):
-                if ligne_idx != i:
-                    matrice_provision[ligne_idx][index_col_choisie] = 0
-
-            # La ligne n'est plus prise en compte
-            lignes_actives.remove(i)
             
-        # Ca me parait être redondant vu qu'on fait la meme chose mais version ligne et colonne... je sais pas si c'est très opti....
-        # En tout cas ça fonctionne je pense que je vais regarder pour améliorer car la j'en bouffe de l'énergie... 
-        #Ttout mon algo me parait long pour rien. C'est une version 1.0 à voir si j'arrive à l'améliorer ou si vous trouvez des trus mieux
         else:
-            j = meilleure_colonne[0]
-            # trouver la ligne active de coût minimal dans la colonne j
-            cout_min = math.inf
-            for k in lignes_actives:
-                c = matrice_cout[k][j]
-                if c < cout_min:
-                    cout_min = c
-                    index_ligne_choisie = k
-
-            offre = matrice_provision[index_ligne_choisie][-1]
-            demande = matrice_provision[-1][j]
-
-            # Comme pour colonne mais version ligne
-            quantite = min(offre, demande)
-
-            # Comme pour la colonne
-            matrice_provision[index_ligne_choisie][j] = quantite
-
-
-            for col_idx in range(len(matrice_provision[0]) -1):
-                if col_idx != j:
-                    matrice_provision[index_ligne_choisie][col_idx] = 0
-                
-            colonnes_actives.remove(j)
+            # La colonne a le delta le plus élevé
+            colonne_choisie = max(delta_colonnes.items(), key=lambda x: x[1])[0]
+            print("\n C'est quoi ça : ", max(delta_colonnes.items(), key=lambda x: x[1]))
 
             
-        matrice_cout_active = []
-        for i in lignes_actives:
-            ligne_active = [matrice_cout[i][j] for j in colonnes_actives]
-            matrice_cout_active.append(ligne_active)
+            # On prend la ligne avec le coût minimal (si il y a 2 delta le meme, on prend celle avec le plus petit)
+            ligne_choisie = min(lignes_actives, key=lambda r: couts[r, colonne_choisie])
+            print("\n C'est quoi ça V2: ", min(lignes_actives, key=lambda r: couts[r, colonne_choisie]))
 
-        # print("\nMatrice provision à itération :", compteur)
-        # for ligne in matrice_provision:
-        #     print(ligne)
-    # print("\nMatrice coûts")
-    # for ligne in matrice_cout_active:
-    #     print(ligne)
+        print("colonnes :", colonne_choisie)
+        
+        # Calcul de la quantité maximale à mettre dans la case choisie
+        quantite = min(total_lignes_restantes[ligne_choisie], total_colonne_restantes[colonne_choisie])
+        resultat[ligne_choisie, colonne_choisie] = quantite
 
+        # Mise à jour des totaux de colonne et ligne
+        total_lignes_restantes[ligne_choisie] -= quantite
+        total_colonne_restantes[colonne_choisie] -= quantite
 
+        # Si lo total est atteind pour la ligne on rempli le reste de zéro
+        if total_lignes_restantes[ligne_choisie] == 0:
+            for col in colonnes_actives:
+                if col != colonne_choisie:
+                    resultat[ligne_choisie, col] = 0
+            lignes_actives.remove(ligne_choisie)
 
-        compteur += 1
+        # Si le total est atteint pour la colonne on rempli le reste de zéro
+        if total_colonne_restantes[colonne_choisie] == 0:
+            for lig in lignes_actives:
+                if lig != ligne_choisie:
+                    resultat[lig, colonne_choisie] = 0
+            colonnes_actives.remove(colonne_choisie)
 
-    return matrice_provision
-
-
-
-
-
-
-
-
-#--------------------------------------------------
-def delta_ligne(matrice_couts, i):
-    ligne = matrice_couts[i]
-    deux_min = sorted(ligne)
-    return deux_min[1] - deux_min[0]
-
-
-def delta_colonne(matrice_couts, j):
-
-    colonne = [ligne[j] for ligne in matrice_couts]
-    deux_min = sorted(colonne)
-    return deux_min[1] - deux_min[0]
+    #Renvoie la matrice finale
+    return resultat.astype(int).tolist()
--- code/main.py
+++ code/main.py
@@ -1,51 +1,71 @@
+import math
 from algos import *
-
-
-def boucle_principale() :
-    # Lire le tableau et stocker en mémoire (à faire par Matthew)
-    
-    matrice_1 = [ # Matrice de test
-        [30,20,100],
-        [10,50,100],
-        [100,100]
-    ]
-    
-
-    choix_algo = demander_algo()
-
-    if(choix_algo == 1) :
-        # Nord-Ouest
-        print("Vous avez choisi Nord-Ouest")
-    else : # choix_algo = 2
-        print("Vous avez choisi Balas-Hammer")
-
+import numpy as np
 
 if __name__ == "__main__" :
-    matrice_couts = [
-        [30, 20, 20],   # ligne 0
-        [10, 50, 20],   # ligne 1
-        [50, 40, 30]    # ligne 2
+    while True:
+
+        matrice_couts = [
+        [30, 10, 10],   # ligne 0
+        [50, 50, 30],   # ligne 1
+        [50, 20, 30]    # ligne 2
     ]
 
 
-    print("\n MATRICE INITIALE COUT")
-    for ligne in matrice_couts:
-        print(ligne)
+        print("\n MATRICE INITIALE COUT")
+        for ligne in matrice_couts:
+            print(ligne)
 
-    matrice_provisions = [
-        [math.inf, math.inf, math.inf, 450], 
-        [math.inf, math.inf, math.inf, 600], 
-        [math.inf, math.inf, math.inf, 350],   
-        [500,      600,       300,    1400]  
-    ]
+        matrice_provisions = [
+            [math.inf, math.inf, math.inf, 450], 
+            [math.inf, math.inf, math.inf, 600], 
+            [math.inf, math.inf, math.inf, 350],   
+            [500,      600,       300,    1400]  
+        ]
 
     
 
-    print("\n MATRICE INITIALE PROIVISONS")
-    for ligne in matrice_provisions:
-        print(ligne)
+        print("\n MATRICE INITIALE PROIVISONS")
+        for ligne in matrice_provisions:
+            print(ligne)
 
-    resultat = balas_hammer(matrice_provisions, matrice_couts)
-    print("\nRÉSULTAT MATRICE PROVISIONS")
-    for ligne in resultat:
-        print(ligne)
+        resultat = balas_hammer(matrice_provisions, matrice_couts)
+        print("\nRÉSULTAT MATRICE PROVISIONS")
+        for ligne in resultat:
+            print(ligne)
+        break
+        # numProblem = choisir_Num_Probleme()
+
+        # #Lecture et stockage de la matrice des coûts associée au numéro de problème choisi.
+        # graphes.lecture_fichier_txt(f"matrices/matrice_{numProblem}.txt")
+        
+        # # Affichage de la matrice des coûts & Provisions x commandes.
+        # graphes.afficher_matrice_constante(graphes.matrice_cout)
+        # graphes.afficher_matrice_constante(graphes.matrice_provisions_x_commandes)
+        
+        # # Demander à l'utilisateur de choisir l'algorithme pour fixer la proposition initiale et l'exécuter.
+        # choix_Algo = choisir_Algo()
+
+        # # Exécution de l'algorithme choisi pour obtenir une proposition de transport.
+        # graphes.matrice_cout, graphes.matrice_provisions_x_commandes = appliquer_Algo(choix_Algo, graphes.matrice_cout, graphes.matrice_provisions_x_commandes)
+        
+        # # Afficher les matrices de PROVISION et de COÛTS CALCULÉES.
+        # afficher_Matrices(graphes.matrice_cout, graphes.matrice_provisions_x_commandes)
+        
+        # solution_courante = graphes.matrice_cout, graphes.matrice_provisions_x_commandes
+        # est_Optimale = False
+
+        # # Dérouler la méthode du marche-pied avec potentiel :
+        # while not est_Optimale:
+        #     # Exécution d'une itération de la méthode du marche-pied avec potentiel.
+        #     solution_courante, est_Optimale = appliquer_MPP(solution_courante)
+
+        # solution_Optimale = solution_courante
+        # # Afficher la proposition de transport optimale, ainsi que son coût.
+        # print("\n--- RÉSULTAT FINAL ---")
+        # print(f"Proposition de transport optimale : {solution_Optimale['proposition']}")
+        # print(f"Coût total optimal : {solution_Optimale['cout_total']}")
+        
+        # # Proposer à l'utilisateur de changer de problème de transport (Fin tant que)
+        # if not rester_Sur_Meme_Probleme():
+        #     break
