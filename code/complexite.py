190157f2701f735923a9d57230d8e0970d4b69b8
Steve KINGSLEY ARULNEASATHAS <steve.kingsleyarulneasathas@edu.univ-paris13.fr>
2025-12-14 16:39:30 +0100

    Ajout de code pour les nuages des sommes; et optimisations

--- code/complexite.py
+++ code/complexite.py
@@ -45,7 +45,7 @@ def generer_probleme_aleatoire(n) :
 
 
 tailles_n_a_tester = [10, 40, 100, 400, 1000, 4000] # 10000 aussi
-tailles_n_a_tester = [1000]
+tailles_n_a_tester = [400]
 
 
 def tests() :
@@ -57,54 +57,65 @@ def tests() :
 
     theta_MP_NO = []
     theta_MP_BH = []
+
+    somme_theta_NO = []
+    somme_theta_BH = []
     
     for taille_probleme in tailles_n_a_tester :
+        # On réilitialise les tableaux à chaque n testé
         theta_NO = []
         theta_BH = []
 
         theta_MP_NO = []
         theta_MP_BH = []
 
+        somme_theta_NO = []
+        somme_theta_BH = []
+
         print(f"n = {taille_probleme}")
         i = 0
         debut_n = time.process_time()
         for i in range (0, 100) :
-            couts_A, probleme_transport_A = generer_probleme_aleatoire(taille_probleme)
-            couts_B = [ligne[:] for ligne in couts_A]
-            probleme_transport_B = [ligne[:] for ligne in probleme_transport_A]
+            couts_NO, probleme_transport_NO = generer_probleme_aleatoire(taille_probleme)
+            couts_BH = [ligne[:] for ligne in couts_NO]
+            probleme_transport_BH = [ligne[:] for ligne in probleme_transport_NO]
 
 
-            debut_A = time.process_time()
-            nord_ouest.algorithme_nord_ouest(probleme_transport_A)
-            fin_A = time.process_time()
-            difference_temps_A = fin_A - debut_A
-            print(f"Problème {i} - Temps CPU utilisé pour N-O : {difference_temps_A} secondes")
-            theta_NO.append(difference_temps_A)
+            debut_NO = time.process_time()
+            nord_ouest.algorithme_nord_ouest(probleme_transport_NO)
+            fin_NO = time.process_time()
+            difference_temps_NO = fin_NO - debut_NO
+            print(f"Problème {i} - Temps CPU utilisé pour N-O : {difference_temps_NO} secondes")
+            theta_NO.append(difference_temps_NO)
 
-            debut_A = time.process_time()
-            couts_potentiels_A = marchepied.marche_pied_potentiel(couts_A, probleme_transport_A)
-            fin_A = time.process_time()
-            difference_temps_A = fin_A - debut_A
-            print(f"Problème {i} - Temps CPU utilisé pour marche-pied après N-O : {difference_temps_A} secondes")
-            theta_MP_NO.append(difference_temps_A)
+            debut_MP_NO = time.process_time()
+            marchepied.marche_pied_potentiel(couts_NO, probleme_transport_NO)
+            fin_MP_NO = time.process_time()
+            difference_temps_MP_NO = fin_MP_NO - debut_MP_NO
+            print(f"Problème {i} - Temps CPU utilisé pour marche-pied après N-O : {difference_temps_MP_NO} secondes")
+            theta_MP_NO.append(difference_temps_MP_NO)
+
+            somme_theta_NO.append(difference_temps_NO + difference_temps_MP_NO)
 
 
 
-            debut_B = time.process_time()
-            balas_np.balas_hammer(probleme_transport_B,couts_B)
-            fin_B = time.process_time()
-            difference_temps_B = fin_B - debut_B
-            print(f"Problème {i} - Temps CPU utilisé pour Balas-Hammer : {difference_temps_B} secondes")
-            theta_BH.append(difference_temps_B)
+            debut_BH = time.process_time()
+            balas_np.balas_hammer(probleme_transport_BH,couts_BH)
+            fin_BH = time.process_time()
+            difference_temps_BH = fin_BH - debut_BH
+            print(f"Problème {i} - Temps CPU utilisé pour Balas-Hammer : {difference_temps_BH} secondes")
+            theta_BH.append(difference_temps_BH)
 
-            debut_B = time.process_time()
-            couts_potentiels_B = marchepied.marche_pied_potentiel(couts_B, probleme_transport_B)
-            fin_B = time.process_time()
-            difference_temps_B = fin_B - debut_B
-            print(f"Problème {i} - Temps CPU utilisé pour marche-pied après Balas-Hammer : {difference_temps_B} secondes")
-            theta_MP_BH.append(difference_temps_B)
+            debut_MP_BH = time.process_time()
+            marchepied.marche_pied_potentiel(couts_BH, probleme_transport_BH)
+            fin_BH = time.process_time()
+            difference_temps_MP_BH = fin_BH - debut_BH
+            print(f"Problème {i} - Temps CPU utilisé pour marche-pied après Balas-Hammer : {difference_temps_MP_BH} secondes")
+            theta_MP_BH.append(difference_temps_MP_BH)
 
-            
+            somme_theta_BH.append(difference_temps_BH + difference_temps_MP_BH)
+
+
             tailles_testees.append(taille_probleme)
 
         fin_n = time.process_time()
@@ -112,6 +123,7 @@ def tests() :
         print(f"Pour n = {taille_probleme}, temps d'exécution total de {diff_n} secondes.")
         
 
+        # # Nuage de points pour la complexité de Nord-Ouest
         plt.scatter(numero_probleme, theta_NO)
         plt.xlabel("Taille n")
         plt.ylabel("Temps (secondes)")
@@ -121,6 +133,7 @@ def tests() :
         # plt.show()
         plt.figure()
 
+        # # Nuage de points pour la complexité de Balas-Hammer
         plt.scatter(numero_probleme, theta_BH)
         plt.xlabel("Taille n")
         plt.ylabel("Temps (secondes)")
@@ -130,6 +143,7 @@ def tests() :
         # plt.show()
         plt.figure()
 
+        # # Nuage de points pour la complexité du marchepied sur la proposition Nord-Ouest
         plt.scatter(numero_probleme, theta_MP_NO)
         plt.xlabel("Taille n")
         plt.ylabel("Temps (secondes)")
@@ -139,6 +153,7 @@ def tests() :
         # plt.show()
         plt.figure()
 
+        # # Nuage de points pour la complexité du marchepied sur la proposition Balas-Hammer
         plt.scatter(numero_probleme, theta_MP_BH)
         plt.xlabel("Taille n")
         plt.ylabel("Temps (secondes)")
@@ -148,10 +163,28 @@ def tests() :
         # plt.show()
         plt.figure()
 
+        # Nuage de points pour la complexité du marchepied sur la proposition Balas-Hammer
+        plt.scatter(numero_probleme, somme_theta_NO)
+        plt.xlabel("Taille n")
+        plt.ylabel("Temps (secondes)")
+        plt.title("Somme Theta Nord-Ouest et marchepied après NO")
+        plt.grid(True)
+        plt.savefig(f"../nuages_points/nuage_points_somme_theta_NO_{taille_probleme}.png", dpi=300, bbox_inches="tight")
+        # plt.show()
+        plt.figure()
+
+        # Nuage de points pour la complexité du marchepied sur la proposition Balas-Hammer
+        plt.scatter(numero_probleme, somme_theta_BH)
+        plt.xlabel("Taille n")
+        plt.ylabel("Temps (secondes)")
+        plt.title("Somme Theta Balas-Hammer et marchepied après BH")
+        plt.grid(True)
+        plt.savefig(f"../nuages_points/nuage_points_somme_theta_BH_{taille_probleme}.png", dpi=300, bbox_inches="tight")
+        # plt.show()
+        plt.figure()
+
         print("")
 
 
 if __name__ == "__main__" :
-    # tests_nord_ouest()
-    # tests_balas_hammer()
     tests()
